861f8b031942ea5d24c7d2f60bcf722140103a5e
EduardoCastro <ecpcastro08@gmail.com>
2025-04-28 11:56:34 -0500

    creando el archivo para manejar variables de entorno

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +82,10 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'littlelemon_db',
-        'USER': 'root',
-        'PASSWORD': 'Samuel@0809',
-        'HOST': 'localhost',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
         'PORT': '3306',
     }
 }
